d3d030837bd0222ecf424c02657fcd569e34c87d
Bart van Grinsven <bartvangrinsven1@hotmail.com>
2023-07-14 15:02:18 +0200

    5. Styling Services section

--- capture2/src/components/AboutSection.js
+++ capture2/src/components/AboutSection.js
@@ -2,13 +2,14 @@ import React from 'react';
 import home1 from '../img/home1.png';
 // Styled
 import styled from 'styled-components';
+import {StyledAbout, StyledDescription, StyledHide, StyledImage} from '../styles'
 
 
 const AboutSection = () => {
     return(
         // Div is replaced by styled component
         <StyledAbout className="about">
-            <div className='description'>
+            <StyledDescription className='description'>
                 <div className='title'>
                     <StyledHide>
                         <h2>We work to make</h2>
@@ -24,7 +25,7 @@ const AboutSection = () => {
                     <p>Contact us for any photografy or videografy ideas that you have. We are profesionals with amazing talent.</p>
                     <button>- Contact Us</button>
                 </div>
-            </div> 
+            </StyledDescription> 
             <StyledImage>
                     <img src={home1} alt="guy with a camera" />
             </StyledImage>
@@ -32,30 +33,6 @@ const AboutSection = () => {
     )
 }
 
-// Styled Components
-// normal css code within js object
-// Start const with capital letter
-const StyledAbout = styled.div`
-    min-height: 90vh;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 5rem 10rem;
-    color: white;
-`
 
-const StyledImage = styled.div`
-    flex: 1;
-    overflow: hidden;
-    img{
-        width: 100%;
-        height: 80vh;
-        object-fit: cover;
-    }    
-`
-
-const StyledHide = styled.div`
-    overflow: hidden;
-`
 
 export default AboutSection;
--- capture2/src/components/ServicesSection.js
+++ capture2/src/components/ServicesSection.js
@@ -5,46 +5,84 @@ import diaphragm from '../img/diaphragm.svg'
 import money from '../img/money.svg'
 import teamwork from '../img/teamwork.svg'
 import home2 from "../img/home2.png";
+// Styles
+import styled from 'styled-components';
+import {StyledAbout, StyledDescription, StyledHide, StyledImage} from '../styles'
+
 
 const ServiceSection = () => {
     return (
-        <div className="services">
-            <div className="description">
+        <StyledServices>
+            <StyledDescription>
                 <h2>High <span>quality</span> services</h2>
-                <div className="cards">
-                    <div className="card">
+                <StyledCards>
+                    <StyledCard>
                         <div className="icon">
                             <img src={clock}/>
                             <h3>Efficient</h3>
                         </div>
                         <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
-                    <div className="card2">
+                    </StyledCard>
+                    <StyledCard>
                         <div className="icon">
                             <img src={teamwork}/>
                             <h3>Teamwork</h3>
                         </div>
                         <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
-                    <div className="card">
+                    </StyledCard>
+                    <StyledCard>
                         <div className="icon">
                             <img src={diaphragm}/>
                             <h3>Pro Grade Gear</h3>
                         </div>
                         <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
-                    <div className="card">
+                    </StyledCard>
+                    <StyledCard>
                         <div className="icon">
                             <img src={money}/>
                             <h3>Affordable</h3>
                         </div>
                         <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
-                </div>
-            </div>
-            <img src={home2} alt="" />
-        </div>
+                    </StyledCard>
+                </StyledCards>
+            </StyledDescription>
+            <StyledImage>
+                <img src={home2}/>
+            </StyledImage>
+        </StyledServices>
     )
 }
+
+// styled(StyledAbout) copies the properties of the styledcomponent (StyledAbout)
+const StyledServices = styled(StyledAbout)`
+    h2{ 
+        padding-bottom: 5rem;
+    }
+    p{
+        width: 70%;
+        padding: 2rem 0rem 4rem 0rem;
+    }
+`;
+
+const StyledCards = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`
+
+const StyledCard = styled.div`
+    flex-basis: 20rem;
+    .icon{
+        display: flex;
+        flex-basis: center;
+        h3{
+            margin-left: 1rem;
+            background: white;
+            color: black;
+            padding: 1rem;
+        }
+    }
+`
+
+
 
 export default ServiceSection
